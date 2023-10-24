443496505b63c58d351acdb941d15ea43bc2f462
elderfieldzeus <elderfieldzeus24@gmail.com>
2023-10-25 03:02:44 +0800

    added browser sync

--- cProjects/AAAtestingTerminalGit.c
+++ cProjects/AAAtestingTerminalGit.c
@@ -7,6 +7,8 @@ int main(){
     printf("\"git status\" - check status\n");
     printf("\"git add .\" or \"git add (name of file)\" - add before commiting\n");
     printf("\'git commit -m \"(message here)\" \' - commit git with message (required message)\n");
-    printf("\"git push\" - push directory to github\n\n");
+    printf("\"git push\" - push directory to github\n");
     printf("\"git pull\" - pull repository from github\n");
+    printf("\'browser-sync start --server --directory --files \"*\" \' - for browser-sync\n");
+    return 0;
 }
