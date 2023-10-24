bb93bc60615b882e28f38886e03d4b2c33a02dfa
elderfieldzeus <elderfieldzeus24@gmail.com>
2023-10-25 02:51:59 +0800

    trying git terminal update

---
+++ cProjects/testingTerminalGit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Different terminal commands (related to git):\n");
+    printf("\"dir\" - open directory\n");
+    printf("\"cd (directory name)\" - change directory\n");
+    printf("\"git status\" - check status\n");
+    printf("\"git add .\" or \"git add (name of file)\" - add before commiting\n");
+    printf("\'git commit -m \"(message here)\" \' - commit git with message (required message)\n");
+    printf("\"git push\" - push directory\n\n");
+}
