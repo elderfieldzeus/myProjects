9c3c766ca9568b8a148c8c1d207436efacdd3032
elderfieldzeus <elderfieldzeus24@gmail.com>
2023-11-02 03:47:21 +0800

    added struct

--- cProjects/spiralMatrix.c
+++ cProjects/spiralMatrix.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+typedef struct{
+    int row;
+    int col;
+}TABLE;
+
 enum direction{
     RIGHT, DOWN, LEFT, UP
 };
 
 int main(){
+    TABLE first, last;
     int n;
     printf("Enter the size of the matrix: ");
     scanf("%d", &n);
-    int arr[n][n], count = 1, i = 0, j = 0, firstRow = 0, lastRow = n-1, firstCol = 0, lastCol = n-1;
+    int arr[n][n], count = 1, i = 0, j = 0;
+    first.row = 0, last.row = n-1, first.col = 0, last.col = n-1;
     enum direction rowCol = RIGHT;
 
     while (count <= n*n){
@@ -16,38 +23,38 @@ int main(){
         count++;
         switch(rowCol){
             case RIGHT: 
-                if(j < lastCol){
+                if(j < last.col){
                     j++;
                 }else{
                     i++;
-                    firstRow++;
+                    first.row++;
                     rowCol = DOWN;
                 }
                 break;
             case DOWN:
-                if(i < lastRow){
+                if(i < last.row){
                     i++;
                 }else{
                     j--;
-                    lastCol--;
+                    last.col--;
                     rowCol = LEFT;
                 }
                 break;
             case LEFT:
-                if(j > firstCol){
+                if(j > first.col){
                     j--;
                 }else{
                     i--;
-                    lastRow--;
+                    last.row--;
                     rowCol = UP;
                 }
                 break;
             case UP:
-                if(i > firstRow){
+                if(i > first.row){
                     i--;
                 }else{
                     j++;
-                    firstCol++;
+                    first.col++;
                     rowCol = RIGHT;
                 }
                 break;
